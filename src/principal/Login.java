07bd0b7edf223c59e88671cd6930058c4645bafe
Lida <Lida@Lida>
2022-11-19 17:30:21 -0500

    Refactorizacion Login

--- src/principal/Login.java
+++ src/principal/Login.java
@@ -9,7 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import persistencia.MedicosPersistencia;
+import persistencia.AdministradorPersistencia;
 
 /**
  *
@@ -20,9 +20,9 @@ public class Login {
     private String usuario; // El usuario sera la cedula
     private String contrasenia;
     private String tipoUsuario;
-    private File archivo;
 
-    //private MedicosPersistencia medicoPersistencia;
+    private AdministradorPersistencia adminPersistencia;
+    
 
     public Login(String usuario, String contrasenia) {
         this.usuario = usuario;
@@ -34,7 +34,7 @@ public class Login {
             System.out.println("Credenciales erroneas");
             return false;
         } else {
-            Administrador admin = obtenerAdmin(usuario);
+            Administrador admin = adminPersistencia.obtenerAdministrador(usuario);
             if (admin == null) {
                 System.out.println("Acceso Denegado");
                 return false;
@@ -49,50 +49,4 @@ public class Login {
         }
     }
 
-    public Administrador obtenerAdmin(String cedula) {
-        this.archivo = new File("db/admins.txt");
-        if (!this.archivo.exists()) {
-            try {
-                this.archivo.createNewFile();
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-                System.exit(0);
-            }
-        }
-        FileReader fileReader = null;
-        BufferedReader br = null;
-        Administrador admin = null;
-        try {
-            fileReader = new FileReader(this.archivo);
-            br = new BufferedReader(fileReader);
-            String linea = br.readLine();
-            while (linea != null) {
-                String cedulaEncontrada = linea.split(",")[0];
-                if (cedulaEncontrada.equals(cedula)) {
-                    String[] datosAdmin = linea.split(",");
-                    admin = new Administrador(datosAdmin[0], datosAdmin[1], Integer.parseInt(datosAdmin[2]), datosAdmin[3], datosAdmin[4]);
-                    return admin;
-                }
-                linea = br.readLine();
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return admin;
-    }
 }
